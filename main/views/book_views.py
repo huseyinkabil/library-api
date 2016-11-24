0a55d7a709dc67245cb563b7a9cb39c90487efc1
Hüseyin Kabil <hsynkabil@gmail.com>
2016-11-24 13:53:51 +0300

    post method of book endpoint is done

--- main/views/book_views.py
+++ main/views/book_views.py
@@ -1,4 +1,6 @@
+from django.db import IntegrityError
 from django.http import HttpResponse, JsonResponse
+from django.utils.crypto import get_random_string
 from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
 
@@ -39,5 +41,37 @@ class BookView(View):
         if result:
             resp['status'] = 200
             resp['result'] = result
+
+        return JsonResponse(resp, status=resp['status'])
+
+    def post(self, request):
+        post = request.POST
+        required_keys = set(['title', 'authors'])
+
+        resp = {
+            'status': 400,
+            'result': 'Missing or wrong parameter!'
+        }
+        if not required_keys.issubset(set(post.keys())):
+            return JsonResponse(resp, status=resp['status'])
+
+        authors_ids = post.get('authors').split(',')
+        authors = Author.objects.filter(pk__in=authors_ids)
+        if len(authors) != len(authors_ids):
+            resp['result'] = 'Some author(s) cannot be found!'
+            return JsonResponse(resp, status=resp['status'])
+
+        try:
+            new_book = Book.objects.create(
+                title=post['title'],
+                lc_classification=get_random_string()
+            )
+        except IntegrityError:
+            resp['result'] = 'An error occured, please try again!'
+            return JsonResponse(resp, status=resp['status'])
+
+        new_book.authors.add(*authors)
+        resp['status'] = 201
+        resp['result'] = Book.objects.get_from_id_with_authors(new_book.id)
 
         return JsonResponse(resp, status=resp['status'])
